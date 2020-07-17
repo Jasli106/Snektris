14a2e2c99b3ea09ac7cbefec942ebf14dbbe41a1
Jasmine <jasmineli106@gmail.com>
2020-07-17 08:07:36 -0400

    Refactored to get rid of the weird game object thing leftover from crafty

--- src/game.js
+++ src/game.js
@@ -1,3 +1,6 @@
+//REFERENCE: https://www.thatsoftwaredude.com/content/6193/coding-the-snake-game-in-javascript
+
+//VARIABLES
 var timer, snake, segment;
 var direction = "up";
 var x = 10;
@@ -7,223 +10,222 @@ var tempDirs = new Array();
 var snakeColor = "#75ba6d";
 var timeInterval = 400;
 
-Game = {
-  //Setting up main grid
-  drawGrid: function() {
-    var body = document.body, table = document.createElement('table');
-    table.setAttribute("id", "grid");
-    for(var i = 0; i < y; i++) { 
-      var row = table.insertRow();
-      for(var j = 0; j < x; j++) {
-        var cell = row.insertCell();
-      }
-      body.appendChild(table);
+//FUNCTIONS
+//Setting up main grid
+function drawGrid() {
+  var body = document.body, table = document.createElement('table');
+  table.setAttribute("id", "grid");
+  for(var i = 0; i < y; i++) { 
+    var row = table.insertRow();
+    for(var j = 0; j < x; j++) {
+      var cell = row.insertCell();
     }
-  },
+    body.appendChild(table);
+  }
+}
 
-  //Drawing blocks
-  drawBlock: function(block) {
+//Drawing blocks
+function drawBlock(block) {
+  var parent = document.getElementById("grid");
+  parent.rows[block.y].cells[block.x].style.backgroundColor = snakeColor;
+}
+
+//Generate snake segment (goal/thing to collect)
+  function createSegment() {
+  var x2 = Math.floor((Math.random() * x));
+  var y2 = Math.floor((Math.random() * y));
+  let valid = true;
+  for(let i = 0; i < snakeBlocks.length; i++) {
+    if(x2 == snakeBlocks[i].x && y2 == snakeBlocks[i].y) {
+      valid = false;
+    }
+  }
+  if(valid==true) {
+    segment = {x: x2, y: y2};
     var parent = document.getElementById("grid");
-    parent.rows[block.y].cells[block.x].style.backgroundColor = snakeColor;
-  },
+    parent.rows[y2].cells[x2].style.backgroundColor = "red";
+  } else {
+    createSegment();
+  }
+}
 
-  //Generate snake segment (goal/thing to collect)
-  createSegment: function() {
-    var x2 = Math.floor((Math.random() * x));
-    var y2 = Math.floor((Math.random() * y));
-    let valid = true;
-    for(let i = 0; i < snakeBlocks.length; i++) {
-      if(x2 == snakeBlocks[i].x && y2 == snakeBlocks[i].y) {
-        valid = false;
-      }
+//When segment collected
+function checkSegment() {
+  if (snake.x == segment.x && snake.y == segment.y) {
+      createSegment();
+      addChild();
+  }
+}
+
+//Check which key was pressed
+function checkKey(e) {
+  e = e || window.event;
+  if (e.keyCode == '38') {
+    // up arrow
+    if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "down")){
+      direction = "up";
     }
-    if(valid==true) {
-      segment = {x: x2, y: y2};
-      var parent = document.getElementById("grid");
-      parent.rows[y2].cells[x2].style.backgroundColor = "red";
-    } else {
-      Game.createSegment();
+  } else if (e.keyCode == '40') {
+    // down arrow
+    if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "up")) {
+      direction = "down";
     }
-  },
-
-  //When segment collected
-  checkSegment: function() {
-    if (snake.x == segment.x && snake.y == segment.y) {
-        Game.createSegment();
-        Game.addChild();
+  } else if (e.keyCode == '37') {
+    // left arrow
+    if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "right")){
+      direction = "left";
     }
-  },
-
-  //Check which key was pressed
-  checkKey: function(e) {
-    e = e || window.event;
-    if (e.keyCode == '38') {
-      // up arrow
-      if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "down")){
-        direction = "up";
-      }
-    } else if (e.keyCode == '40') {
-      // down arrow
-      if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "up")) {
-        direction = "down";
-      }
-    } else if (e.keyCode == '37') {
-      // left arrow
-      if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "right")){
-        direction = "left";
-      }
-    } else if (e.keyCode == '39') {
-      // right arrow
-      if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "left")){
-        direction = "right";
-      }
+  } else if (e.keyCode == '39') {
+    // right arrow
+    if (snakeBlocks.length == 1 || (snakeBlocks.length > 1 && direction != "left")){
+      direction = "right";
     }
-  },
+  }
+}
 
-  // clear the current block
-  clearBlock: function(block) {
-    var parent = document.getElementById("grid");
-    parent.rows[block.y].cells[block.x].style.backgroundColor = "white";
-  },
+// clear the current block
+function clearBlock(block) {
+  var parent = document.getElementById("grid");
+  parent.rows[block.y].cells[block.x].style.backgroundColor = "white";
+}
 
-  //Handle snake movement
-  move: function() {
-    //Clear old snake head block
-    Game.clearBlock(snake)
+//Handle snake movement
+function move() {
+  //Clear old snake head block
+  clearBlock(snake)
 
-    //Update the snake body and check if head runs into tail
-    Game.checkCollision();
-    Game.updateSnake(snakeBlocks.length);
+  //Update the snake body and check if head runs into tail
+  checkCollision();
+  updateSnake(snakeBlocks.length);
 
-    // determines the new x and y values when changing direction
-    snake.dir = direction;
+  // determines the new x and y values when changing direction
+  snake.dir = direction;
 
-    switch(direction) {
+  switch(direction) {
+    case "up":
+      snake.y--;
+      break;
+
+    case "down":
+      snake.y++;
+      break;
+
+    case "left":
+      snake.x--;
+      break;
+
+    case "right":
+      snake.x++;
+      break;
+  }
+
+  // edge detection. game ends if this happens
+  if (snake.x < 0 || snake.y < 0 || snake.x >= x || snake.y >= y) {
+    document.getElementById("title").innerHTML = "Lost";
+    clearInterval(timer);
+  } else {
+    drawBlock(snake);
+    checkSegment();
+  }
+  //Set timer interval
+  timeInterval = 400-(snakeBlocks.length*5);
+  if (timeInterval < 100) {
+    timeInterval = 100;
+  }
+  console.log(timeInterval);
+}
+
+//Adding a section to the snake
+function addChild() {
+  let endBlock = snakeBlocks[snakeBlocks.length-1];
+  var seg = {
+    //calculate coordinates based on direction
+    y: endBlock.y,
+    x: endBlock.x,
+    dir: endBlock.dir
+  }
+  switch(seg.dir) {
+    case "up":
+      seg.y = endBlock.y + 1;
+      break;
+
+    case "down":
+      seg.y = endBlock.y - 1;
+      break;
+
+    case "left":
+      seg.x = endBlock.x + 1;
+      break;
+
+    case "right":
+      seg.x = endBlock.x - 1;
+      break;
+  }
+  snakeBlocks.push(seg);    
+}
+
+function updateSnake(len) {
+  //Add all directions to tempDirs
+  for(let i = 0; i < len; i++) {
+    tempDirs.push(snakeBlocks[i].dir);
+  }
+
+  //Update snake segment directions
+  for(let i = 1; i < len; i++) {
+    clearBlock(snakeBlocks[i])
+    snakeBlocks[i].dir = tempDirs[i-1];
+  }
+  tempDirs = [];
+
+  //Update snake segment coordinates
+  for(let i = 1; i < len; i++) {
+    switch(snakeBlocks[i].dir) {
       case "up":
-        snake.y--;
+        snakeBlocks[i].y--;
         break;
 
       case "down":
-        snake.y++;
+        snakeBlocks[i].y++;
         break;
 
       case "left":
-        snake.x--;
+        snakeBlocks[i].x--;
         break;
 
       case "right":
-        snake.x++;
+        snakeBlocks[i].x++;
         break;
     }
+  }
+  //Visuals
+  for(let i = 1; i < len; i++) {
+    drawBlock(snakeBlocks[i])
+  }
+}
 
-    // edge detection. game ends if this happens
-    if (snake.x < 0 || snake.y < 0 || snake.x >= x || snake.y >= y) {
+//Check if snake collides with tail
+function checkCollision() {
+  for(let i = 1; i < snakeBlocks.length; i++) {
+    if((snakeBlocks[i].x == snake.x) && (snakeBlocks[i].y == snake.y)) {
       document.getElementById("title").innerHTML = "Lost";
       clearInterval(timer);
-    } else {
-      Game.drawBlock(snake);
-      Game.checkSegment();
     }
-    //Set timer interval
-    timeInterval = 400-(snakeBlocks.length*5);
-    if (timeInterval < 100) {
-      timeInterval = 100;
-    }
-    console.log(timeInterval);
-  },
-
-  //Adding a section to the snake
-  addChild: function() {
-    let endBlock = snakeBlocks[snakeBlocks.length-1];
-    var seg = {
-      //calculate coordinates based on direction
-      y: endBlock.y,
-      x: endBlock.x,
-      dir: endBlock.dir
-    }
-    switch(seg.dir) {
-      case "up":
-        seg.y = endBlock.y + 1;
-        break;
-
-      case "down":
-        seg.y = endBlock.y - 1;
-        break;
-
-      case "left":
-        seg.x = endBlock.x + 1;
-        break;
-
-      case "right":
-        seg.x = endBlock.x - 1;
-        break;
-    }
-    snakeBlocks.push(seg);    
-  },
-
-  updateSnake: function(len) {
-    //Add all directions to tempDirs
-    for(let i = 0; i < len; i++) {
-      tempDirs.push(snakeBlocks[i].dir);
-    }
-
-    //Update snake segment directions
-    for(let i = 1; i < len; i++) {
-      Game.clearBlock(snakeBlocks[i])
-      snakeBlocks[i].dir = tempDirs[i-1];
-    }
-    tempDirs = [];
-
-    //Update snake segment coordinates
-    for(let i = 1; i < len; i++) {
-      switch(snakeBlocks[i].dir) {
-        case "up":
-          snakeBlocks[i].y--;
-          break;
-
-        case "down":
-          snakeBlocks[i].y++;
-          break;
-
-        case "left":
-          snakeBlocks[i].x--;
-          break;
-
-        case "right":
-          snakeBlocks[i].x++;
-          break;
-      }
-    }
-    //Visuals
-    for(let i = 1; i < len; i++) {
-      Game.drawBlock(snakeBlocks[i])
-    }
-  },
-
-  //Check if snake collides with tail
-  checkCollision: function() {
-    for(let i = 1; i < snakeBlocks.length; i++) {
-      if((snakeBlocks[i].x == snake.x) && (snakeBlocks[i].y == snake.y)) {
-        document.getElementById("title").innerHTML = "Lost";
-        clearInterval(timer);
-      }
-    }
-  },
-
-  //Game timer starts
-  start: function() {
-    Game.createSegment();
-    document.onkeydown = Game.checkKey;
-    timer = setInterval(function(){Game.move();}, timeInterval);
-    snakeBlocks.push(snake);
-  },
-
-  // Initialize and start our game
-  load: function() {
-    Game.drawGrid();
-    var parent = document.getElementById("grid");
-    snake = {y: Math.floor(Math.random() * y), x: Math.floor(Math.random() * x), dir: direction};
-    Game.drawBlock(snake);
-    Game.start();
   }
+}
+
+//Game timer starts
+function start() {
+  createSegment();
+  document.onkeydown = checkKey;
+  timer = setInterval(function(){move();}, timeInterval);
+  snakeBlocks.push(snake);
+}
+
+// Initialize and start our game
+function load() {
+  drawGrid();
+  var parent = document.getElementById("grid");
+  snake = {y: Math.floor(Math.random() * y), x: Math.floor(Math.random() * x), dir: direction};
+  drawBlock(snake);
+  start();
 }
